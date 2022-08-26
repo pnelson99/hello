d94bd9df53b531ccb0f64e380b96c8cb5ba1863a
Cameron <clyons13@huskers.unl.edu>
2022-08-26 22:12:01 +0000

    added name and major to partner's repository

--- hello.c
+++ hello.c
@@ -9,5 +9,8 @@ int main(int argc, char **argv)  {
 printf("Hello, I'm Peyton Nelson!\n");
 printf("Major is Computer Engineering!\n");
 
+printf("Cameron Lyons!)\n");
+printf("Undeclared\n");
+
     return 0;
 }
