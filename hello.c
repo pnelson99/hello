1183bfab1e1ec1caa6a4538b3316171ce08e787a
Peyotn Nelson <pnelson21@husker.unl.edu>
2022-08-26 04:33:33 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv)  {
+
+printf("Hello, I'm Peyton Nelson!\n");
+printf("Major is Computer Engineering!\n");
+
+    return 0;
+}
